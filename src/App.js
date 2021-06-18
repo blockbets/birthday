76afd4868c254198df0273beb65de9f5cdebd4ac
blockbets <78051866+blockbets@users.noreply.github.com>
2021-06-18 18:08:56 +0100

    3rd commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
         <Switch>
           <Route path="/" exact>
-            <Countdown />
+            <Presents />
           </Route>
           <Route path="/quiz">
             <Quiz />
